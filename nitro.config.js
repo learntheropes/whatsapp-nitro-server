5ac590283f61ea2e7ca6ddfefba3aefb400c7ca2
learntheropes <giovanni@learntheropes.xyz>
2023-09-13 04:14:40 +0200

    remove puppeteerExecutablePath

--- nitro.config.js
+++ nitro.config.js
@@ -5,7 +5,6 @@ export default defineNitroConfig({
     telegramToken: process.env.TELEGRAM_TOKEN,
     telegramChatId: process.env.TELEGRAM_CHAT_ID,
     whatsappAdmin: process.env.WHATSAPP_ADMIN,
-    whatsappForward : process.env.WHATSAPP_FORWARD,
-    puppeteerExecutablePath: process.env.PUPPETEER_EXECUTABLE_PATH
+    whatsappForward : process.env.WHATSAPP_FORWARD
   },
 });
